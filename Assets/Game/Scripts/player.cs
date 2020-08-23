8e46069387d7ae9478dbfaf096a4f587070252dc
Diogo Costa <37339406+diogocsbr@users.noreply.github.com>
2020-08-23 02:16:56 -0300

    Background, Player, movimentação
    Background, Player, movimentação, contraint de posicionamento

---
+++ Assets/Game/Scripts/player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimento();
+    }
+
+    private void Movimento() {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+
+        //meio tela e naão sai por baixo
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.2f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+
+        //if (transform.position.x > 8)
+        //{
+        //    transform.position = new Vector3(8, transform.position.y, 0);
+        //}
+        //else if (transform.position.x < -8)
+        //{
+        //    transform.position = new Vector3(-8, transform.position.y, 0);
+        //}
+
+
+        if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.5f)
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
+    }
+}
